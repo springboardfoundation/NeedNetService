56c7fb679332eacb2a4af839164d4057f3fe62c9
Reehan <reehan.lalkhan@gmail.com>
2020-08-04 19:59:27 +0530

    NeedNet micro-service for combined response of needs and users

---
+++ neednet-service/src/main/java/org/springboardfoundation/neednetservice/NeednetServiceApplication.java
@@ -0,0 +1,13 @@
+package org.springboardfoundation.neednetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeednetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeednetServiceApplication.class, args);
+	}
+
+}
---
+++ neednet-service/src/test/java/org/springboardfoundation/neednetservice/NeednetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springboardfoundation.neednetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeednetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
