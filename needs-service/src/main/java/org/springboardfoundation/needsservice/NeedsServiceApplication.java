4aef0189fc64c21cdec4831e42426c60693f3e1b
Reehan <reehan.lalkhan@gmail.com>
2020-08-04 19:59:39 +0530

    Needs micro-service to handle needs data

---
+++ needs-service/src/main/java/org/springboardfoundation/needsservice/NeedsServiceApplication.java
@@ -0,0 +1,13 @@
+package org.springboardfoundation.needsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeedsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeedsServiceApplication.class, args);
+	}
+
+}
---
+++ needs-service/src/test/java/org/springboardfoundation/needsservice/NeedsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springboardfoundation.needsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeedsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
